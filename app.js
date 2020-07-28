b2d937f7d007ef784710bc343df634a5d1a671b4
Yujin Shin <yujincat@github.com>
2020-07-28 23:22:01 +0900

    Add a webhook

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
-var vfile = require('to-vfile')
-var report = require('vfile-reporter')
-var unified = require('unified')
-var english = require('retext-english')
-var stringify = require('retext-stringify')
-var passive = require('retext-passive')
+// semantic
+import report from 'vfile-reporter'
+import unified from 'unified'
+import english from 'retext-english'
+import stringify from 'retext-stringify'
+import passive from 'retext-passive'
 
-unified()
-  .use(english)
-  .use(passive)
-  .use(stringify)
-  .process(vfile.readSync('passive.txt'), function (err, file) {
-    const result = report(err || file)
-    console.error(result)
-  })
+// webhook
+import express from 'express'
+const app = express()
+import bodyParser from 'body-parser'
+
+const PORT = 3000
+
+app.use(bodyParser.json())
+
+app.post('/comment', function (req, res) {
+  const jsonBody = req.body
+  const comment = jsonBody.comment.body
+  const commentAction = jsonBody.action
+  if (comment && (commentAction === 'created' || commentAction === 'edited')) {
+    const result = passiveAnalysis(comment)
+    // const commentUrl = jsonBody.comment.html_url
+    // later we want to make a row of comment, result, and url and report it once a week or something like that..
+  }
+
+  res.status(200).end('ok')
+})
+
+process.on('exit', function () {
+})
+
+var server = app.listen(PORT, function () {
+  console.log(`Listening on port ${server.address().port}`)
+})
+
+function passiveAnalysis(content) {
+  return unified()
+    .use(english)
+    .use(passive)
+    .use(stringify)
+    .process(content, function (err, file) {
+      const result = report(err || file)
+      console.log(result)
+      return result
+    })
+}
+
+
